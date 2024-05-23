763991ceb25b1da612e33570e1645be8baa046f6
jananiram05 <jpjanani5@gmail.com>
2024-05-23 11:09:47 -0400

    updated LoginPage

--- crmSelenium/src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ crmSelenium/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -2,6 +2,7 @@ package com.crm.qa.testcases;
 
 import org.testng.Assert;
 
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,6 +11,8 @@ import com.crm.qa.ExtentReportListener.ExtentReportsUtility;
 import com.crm.qa.base.BaseTest;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
+
+//LoginPage
 
 public class LoginPageTest extends BaseTest {
 	LoginPage loginPage;
